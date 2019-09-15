bb25d9e9767575adf94d0803c788cf93b642940b
stud543 <1@gmail.com>
2019-09-15 10:06:15 +0300

    C3

--- one.py
+++ one.py
@@ -6,3 +6,22 @@ for i in range(n):
     a.append(random.randint(-10,10))
 
 print(a)
+
+neg=[]
+zero=[]
+pos=[]
+
+for el in a:
+    if el<0:
+        neg.append(el)
+    if el==0:
+        zero.append(el)
+    if el>0:
+        pos.append(el)
+
+print("Отрицательные")
+print(neg)
+print("Нули")
+print(zero)
+print("Положительные")
+print(pos)
